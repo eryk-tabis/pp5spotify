349a4df9b5502eb7971d00e77b25d09b084ccd7f
Eryk Tabis <s222765@student.uek.krakow.pl>
2023-11-15 16:43:35 +0100

    add all necessary files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchMusicComponent } from './search-music/search-music.component';
+import { SearchArtistComponent } from './search-artist/search-artist.component';
+import {RegisterComponent} from "./register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search-music', component: SearchMusicComponent },
+  { path: 'search-artist', component: SearchArtistComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/search-music', pathMatch: 'full' } // Default route
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchMusicComponent } from './search-music/search-music.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchArtistComponent } from './search-artist/search-artist.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatRadioModule} from "@angular/material/radio";
+import {MatInputModule} from "@angular/material/input";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSelectModule} from "@angular/material/select";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatButtonModule} from "@angular/material/button";
+
+const appRoutes: Routes = [
+  { path: '/search-music', component: SearchMusicComponent },
+  { path: '/search-artist', component: SearchArtistComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: '/search-music', pathMatch: 'full' }, // Default route,
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchMusicComponent,
+    RegisterComponent,
+    SearchArtistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatRadioModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
